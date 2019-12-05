1c5097c54ee57240cf922868f4c9f7f18de00aa3
momoko <1165874814@qq.com>
2019-12-05 17:50:11 +0800

    12月5号学习成果

---
+++ src/test/java/LearnCalenderTest.java
@@ -0,0 +1,28 @@
+import date_and_time.LearnCalender;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by momoko on 2019/12/5
+ *
+ * @author momoko
+ */
+public class LearnCalenderTest {
+    @Test
+    void testNextDayWeekStr() {
+        Date date = new Date();
+        String nextDayWeekStr = LearnCalender.getNextDayWeekStr(date);
+        Assertions.assertEquals("Fri", nextDayWeekStr);
+
+        date.setTime(date.getTime() + 24*60*60*1000);
+        nextDayWeekStr = LearnCalender.getNextDayWeekStr(date);
+        Assertions.assertEquals("Sat", nextDayWeekStr);
+
+        date.setTime(date.getTime() + 2 * 24*60*60*1000);
+        nextDayWeekStr = LearnCalender.getNextDayWeekStr(date);
+        Assertions.assertEquals("Mon", nextDayWeekStr);
+    }
+}

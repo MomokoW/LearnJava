a6897d29d317db8e90d72c8e09c33d313ae31a5f
momoko <1165874814@qq.com>
2019-12-26 15:22:58 +0800

    Add thread learning

---
+++ src/main/java/thread/LFuture.java
@@ -0,0 +1,42 @@
+package thread;
+
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.*;
+
+/**
+ * Created by momoko on 2019/12/26
+ *
+ * @author momoko
+ */
+
+/**
+ * 一个Future<V>接口表示一个未来可能会返回的结果，它定义的方法有：
+ *
+ * get()：获取结果（可能会等待）
+ * get(long timeout, TimeUnit unit)：获取结果，但只等待指定的时间；
+ * cancel(boolean mayInterruptIfRunning)：取消当前任务；
+ * isDone()：判断任务是否已完成。
+ */
+public class LFuture {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(4);
+        Future<?> future = es.submit(new Task("601857"));
+        System.out.println(future.get());
+        es.shutdown();
+    }
+
+}
+class Task1 implements Callable<BigDecimal> {
+
+    public Task1(String code) {
+    }
+
+    @Override
+    public BigDecimal call() throws Exception {
+        Thread.sleep(1000);
+        double d = 5 + Math.random() * 20;
+        return new BigDecimal(d).setScale(2, RoundingMode.DOWN);
+    }
+}
---
+++ src/test/java/VersionCheckTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import solutions.VersionCheck;
+
+/**
+ * Created by momoko on 2019/12/26
+ *
+ * @author momoko
+ */
+public class VersionCheckTest {
+
+    @Test
+    void testVersionCompares() {
+        VersionCheck versionCheck = new VersionCheck();
+
+        int i = versionCheck.compareVersion("1.0.1", "1.1.1");
+        Assertions.assertEquals(-1, i);
+
+        i = versionCheck.compareVersion("1.1", "1.1.0");
+        Assertions.assertEquals(0, i);
+
+        i = versionCheck.compareVersion("1.3.1alpha", "1.3.0");
+        Assertions.assertEquals(0, i);
+
+        i = versionCheck.compareVersion("1.2.14", "1.2.1");
+        Assertions.assertEquals(13, i);
+
+        i = versionCheck.compareVersion("2.2.14", "1.2.1");
+        Assertions.assertEquals(1, i);
+
+        i = versionCheck.compareVersion("2.2.14", "2.2.14");
+        Assertions.assertEquals(1, i);
+
+
+    }
+
+    @Test
+    void testNumberCheck() {
+        VersionCheck versionCheck = new VersionCheck();
+        boolean b = versionCheck.isNumber("");
+        Assertions.assertFalse(b);
+        b = versionCheck.isNumber("111a");
+        Assertions.assertFalse(b);
+        b = versionCheck.isNumber("1");
+        Assertions.assertTrue(b);
+        b = versionCheck.isNumber("1.0");
+        Assertions.assertFalse(b);
+        b = versionCheck.isNumber("1alpha");
+        Assertions.assertFalse(b);
+
+
+    }
+}
